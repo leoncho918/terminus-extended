89d3b1936d004c7250dfe986695c249ace7f6610
Brooke Kuhlmann <brooke@alchemists.io>
2026-03-19 14:12:53 -0600

    Updated screen converter to use color and monochrome converters
    Necessary to fold in the new converters with improved image processing. The original converters (i.e. png and bmp) are no longer needed and will soon be removed.
    
    Milestone: minor

--- app/aspects/screens/converter.rb
+++ app/aspects/screens/converter.rb
@@ -1,23 +1,13 @@
 # frozen_string_literal: true
 
-require "dry/monads"
-require "initable"
-
 module Terminus
   module Aspects
     module Screens
       # Converts to greyscale image based on MIME Type.
       class Converter
-        include Deps["aspects.screens.converters.bmp", "aspects.screens.converters.png"]
-        include Dry::Monads[:result]
+        include Deps["aspects.screens.converters.color", "aspects.screens.converters.monochrome"]
 
-        def call mold
-          case mold
-            in mime_type: "image/bmp" then bmp.call mold
-            in mime_type: "image/png" then png.call mold
-            else Failure "Unsupported MIME Type: #{mold.mime_type}."
-          end
-        end
+        def call(mold) = mold.color? ? color.call(mold) : monochrome.call(mold)
       end
     end
   end
--- spec/app/aspects/screens/converter_spec.rb
+++ spec/app/aspects/screens/converter_spec.rb
@@ -17,37 +17,51 @@ RSpec.describe Terminus::Aspects::Screens::Converter do
                  output_path: temp_dir.join("test.png")
     end
 
-    it "converts to 1-bit BMP image" do
-      converter.call mold.with(mime_type: "image/bmp")
-      image = MiniMagick::Image.open mold.output_path
-
-      expect(image).to have_attributes(
-        dimensions: [800, 480],
-        exif: {},
-        type: "BMP3",
-        data: hash_including("depth" => 1)
+    it "converts to color image" do
+      converter.call mold.with(
+        mode: :dither,
+        grays: 2,
+        bit_depth: 2,
+        color_codes: %w[#000000 #FF0000 #FFFFFF]
       )
-    end
 
-    it "converts to 1-bit PNG image" do
-      converter.call mold
       image = MiniMagick::Image.open mold.output_path
 
       expect(image).to have_attributes(
         dimensions: [800, 480],
         exif: {},
         type: "PNG",
-        data: hash_including("depth" => 1)
+        data: hash_including(
+          "colormap" => %w[#000000FF #FFFFFFFF],
+          "colorspace" => "Gray",
+          "depth" => 1,
+          "mimeType" => "image/png",
+          "type" => "Grayscale"
+        )
+      )
+    end
+
+    it "converts to monochrome image" do
+      converter.call mold
+
+      image = MiniMagick::Image.open mold.output_path
+
+      expect(image).to have_attributes(
+        dimensions: [800, 480],
+        exif: {},
+        type: "PNG",
+        data: hash_including(
+          "colormap" => %w[#000000FF #FFFFFFFF],
+          "colorspace" => "Gray",
+          "depth" => 1,
+          "mimeType" => "image/png",
+          "type" => "Grayscale"
+        )
       )
     end
 
     it "answers image path" do
       expect(converter.call(mold)).to be_success(mold.output_path)
-    end
-
-    it "answers failure for invalid MIME Type" do
-      result = converter.call mold.with(mime_type: "image/tiff")
-      expect(result).to be_failure("Unsupported MIME Type: image/tiff.")
     end
   end
 end
