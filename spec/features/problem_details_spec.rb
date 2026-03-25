289ac34947da5fa3f4cfad884914997a30db2aeb
Brooke Kuhlmann <brooke@alchemists.io>
2026-03-25 09:51:38 -0600

    Removed database metadata from problem details feature spec
    No database is needed for this spec, so safe to remove.
    
    Milestone: patch

--- spec/features/problem_details_spec.rb
+++ spec/features/problem_details_spec.rb
@@ -2,7 +2,7 @@
 
 require "hanami_helper"
 
-RSpec.describe "Problem Details", :db do
+RSpec.describe "Problem Details" do
   it "renders page" do
     visit routes.path(:problem_details)
     expect(page).to have_content("Problem Details")
